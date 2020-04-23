b313e0d53931418e3da2a692366770e773c247f4
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-04-23 15:29:35 +0600

    Delete methods tests

--- enumerables.rb
+++ enumerables.rb
@@ -7,18 +7,12 @@ module Enumerable
     end
   end
 
-  puts [1, 2, 3, 4, 5].my_each
-
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
     size.times do |i|
       yield(self[i], i)
     end
-  end
-
-  [1, 2, 3, 4, 5].my_each_with_index do |_num, index|
-    print index
   end
 
   def my_select
@@ -31,18 +25,12 @@ module Enumerable
     array
   end
 
-  [1, 2, 3, 4, 5].my_select
-
   def my_all?
     my_each do |e|
       return false unless yield(e)
     end
     true
   end
-
-  # [1,2,3,4,5].my_all? do |num|
-  #    num>0
-  # end
 
   def my_any?
     my_each do |e|
@@ -51,20 +39,12 @@ module Enumerable
     false
   end
 
-  # [1,2,3,4,5].my_any? do |num|
-  #    num == 2
-  # end
-
   def my_none?
     my_each do |e|
       return false if yield(e)
     end
     true
   end
-
-  # [1,2,3,4,5].my_none? do |num|
-  #    num == 5
-  # end
 
   def my_count
     count = 0
@@ -73,8 +53,4 @@ module Enumerable
     end
     count
   end
-
-  # [1,2,3,nil,5].my_count do |num|
-  #    num == nil
-  # end
 end
